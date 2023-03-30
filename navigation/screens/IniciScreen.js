a27778eb4f93e19202e85ad9ca417437ce2ba975
magadu22daw <magadu22daw@proton.me>
2023-03-30 12:59:28 +0200

    inici bonito :)

--- navigation/screens/IniciScreen.js
+++ navigation/screens/IniciScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 export default function IniciScreen({ navigation }) {
     return (
         <ScrollView style={styles.container}>
+            
             <Text style={styles.title}>Benvingut a les dades de pobresa mundial!</Text>
-            <Text style={styles.description}>Aquesta aplicació mostra dades de pobresa de tot el món per ajudar-te a comprendre millor el problema global de la pobresa. Pots seleccionar un país per veure les dades de pobresa d'aquell país o. Si necessites ajuda per utilitzar l'aplicació, prem el botó d'ajuda per obtenir més informació.</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Taula')}>
+            <Text style={styles.description}>Aquesta aplicació mostra dades de pobresa de tot el món per ajudar-te a comprendre millor el problema global de la pobresa. Pots seleccionar un país per veure les dades de pobresa d'aquell país.</Text>
+            <Image source={require('./img/pobreza.jpg')} style={{ width: 355, height: 175, marginBottom: 20,borderRadius: 10}}/>
+            <Image source={require('./img/pobreza2.jpg')} style={{ width: 355, height: 175, marginBottom: 20,borderRadius: 10}}/>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Dades')}>
                 <Text style={styles.buttonText}>Comença</Text>
             </TouchableOpacity>
         </ScrollView>
     );
 };
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
